f090b572795ec08d2d68acc70e7fd13bffeeae66
PatrickO10 <patrickorth10@gmail.com>
2018-04-25 14:54:39 -0500

    adds information about the app in the readme

---
+++ src/utils/data.js
@@ -0,0 +1,26 @@
+const numberSystem = {
+    'pao00-09': {
+        00: { initials: 'OO', name: 'Ozzy Osbourne', action: 'biting', object: 'bat' },
+        01: { initials: 'OA', name: 'Oakley Annie', action: 'shooting gun', object: 'target' },
+        02: { initials: 'OB', name: 'Orlando Bloom', action: 'shooting bow', object: 'arrow' },
+        03: { initials: 'OC', name: 'Original Content', action: 'posting', object: 'meme' },
+        04: { initials: 'OD', name: 'Over Dose', action: 'injecting', object: 'needle' },
+        05: { initials: 'OE', name: 'Omar Epps', action: 'hitchhiking', object: 'towel' },
+        06: { initials: 'OS', name: 'Of Shire (Frodo)', action: 'stabbing', object: 'orc' },
+        07: { initials: 'OG', name: 'Oscar Grouch', action: 'popping-out', object: 'trash can' },
+        08: { initials: 'OH', name: 'Of Havana', action: 'singing with', object: 'microphone' },
+        09: { initials: 'ON', name: 'Oscar Nunez', action: 'calculating with', object: 'calculator' }
+    }
+    'pao10-19': {
+    	10: { initials: 'AO', name: 'Ashley Olsen', action: 'puking', object: 'puke' },
+    	11: { initials: 'AA', name: 'Andre Agassi', action: 'swinging', object: 'tennis racquet' },
+    	12: { initials: 'AB', name: 'Alexander Bell', action: 'answering', object: 'telephone' },
+    	13: { initials: 'AC', name: 'Al capone', action: 'smoking', object: 'cigar' },
+    	14: { initials: 'AD', name: 'Alexandra Daddario', action: 'revealing', object: 'boobs' },
+    	15: { initials: 'AE', name: 'Albert Einstein', action: 'sticking out', object: 'tongue' },
+    	16: { initials: 'AS', name: 'Adam Sandler', action: 'chugging', object: 'beer' },
+    	17: { initials: 'AG', name: 'Ariana Grande', action: 'riding', object: 'stationary bike' },
+    	18: { initials: 'AH', name: 'Adolf Hitler', action: 'saluting', object: 'Nazi Flag' },
+    	19: { initials: 'AN', name: 'Alfred Nobel', action: 'blowing up', object: 'dynamite' }
+    }
+}
