4b1c538639771a7395e1d740f238323ad19adbc5
PatrickO10 <patrickorth10@gmail.com>
2018-04-27 22:58:29 -0500

    adds more data

--- src/utils/data.js
+++ src/utils/data.js
@@ -22,5 +22,101 @@ export const numberSystem = {
         '17': { num: '17', initials: 'AG', name: 'Ariana Grande', action: 'riding', object: 'stationary bike' },
         '18': { num: '18', initials: 'AH', name: 'Adolf Hitler', action: 'saluting', object: 'Nazi Flag' },
         '19': { num: '19', initials: 'AN', name: 'Alfred Nobel', action: 'blowing up', object: 'dynamite' }
+    },
+    'pao20-29': {
+        '20': { num: '20', initials: 'B0', name: '', action: '', object: '' },
+        '21': { num: '21', initials: 'BA', name: '', action: '', object: '' },
+        '22': { num: '22', initials: 'BB', name: '', action: '', object: '' },
+        '23': { num: '23', initials: 'BC', name: '', action: '', object: '' },
+        '24': { num: '24', initials: 'BD', name: '', action: '', object: '' },
+        '25': { num: '25', initials: 'BE', name: '', action: '', object: '' },
+        '26': { num: '26', initials: 'BS', name: '', action: '', object: '' },
+        '27': { num: '27', initials: 'BG', name: '', action: '', object: '' },
+        '28': { num: '28', initials: 'BH', name: '', action: '', object: '' },
+        '29': { num: '29', initials: 'BN', name: '', action: '', object: '' }
+    },
+    'pao30-39': {
+        '30': { num: '30', initials: '', name: '', action: '', object: '' },
+        '31': { num: '31', initials: '', name: '', action: '', object: '' },
+        '32': { num: '32', initials: '', name: '', action: '', object: '' },
+        '33': { num: '33', initials: '', name: '', action: '', object: '' },
+        '34': { num: '34', initials: '', name: '', action: '', object: '' },
+        '35': { num: '35', initials: '', name: '', action: '', object: '' },
+        '36': { num: '36', initials: '', name: '', action: '', object: '' },
+        '37': { num: '37', initials: '', name: '', action: '', object: '' },
+        '38': { num: '38', initials: '', name: '', action: '', object: '' },
+        '39': { num: '39', initials: '', name: '', action: '', object: '' }
+    },
+    'pao40-49': {
+        '40': { num: '40', initials: '', name: '', action: '', object: '' },
+        '41': { num: '41', initials: '', name: '', action: '', object: '' },
+        '42': { num: '42', initials: '', name: '', action: '', object: '' },
+        '43': { num: '43', initials: '', name: '', action: '', object: '' },
+        '44': { num: '44', initials: '', name: '', action: '', object: '' },
+        '45': { num: '45', initials: '', name: '', action: '', object: '' },
+        '46': { num: '46', initials: '', name: '', action: '', object: '' },
+        '47': { num: '47', initials: '', name: '', action: '', object: '' },
+        '48': { num: '48', initials: '', name: '', action: '', object: '' },
+        '49': { num: '49', initials: '', name: '', action: '', object: '' }
+    },
+    'pao50-59': {
+        '50': { num: '50', initials: '', name: '', action: '', object: '' },
+        '51': { num: '51', initials: '', name: '', action: '', object: '' },
+        '52': { num: '52', initials: '', name: '', action: '', object: '' },
+        '53': { num: '53', initials: '', name: '', action: '', object: '' },
+        '54': { num: '54', initials: '', name: '', action: '', object: '' },
+        '55': { num: '56', initials: '', name: '', action: '', object: '' },
+        '56': { num: '56', initials: '', name: '', action: '', object: '' },
+        '57': { num: '57', initials: '', name: '', action: '', object: '' },
+        '58': { num: '58', initials: '', name: '', action: '', object: '' },
+        '59': { num: '59', initials: '', name: '', action: '', object: '' }
+    },
+    'pao60-69': {
+        '60': { num: '60', initials: '', name: '', action: '', object: '' },
+        '61': { num: '61', initials: '', name: '', action: '', object: '' },
+        '62': { num: '62', initials: '', name: '', action: '', object: '' },
+        '63': { num: '63', initials: '', name: '', action: '', object: '' },
+        '64': { num: '64', initials: '', name: '', action: '', object: '' },
+        '65': { num: '65', initials: '', name: '', action: '', object: '' },
+        '66': { num: '66', initials: '', name: '', action: '', object: '' },
+        '67': { num: '67', initials: '', name: '', action: '', object: '' },
+        '68': { num: '68', initials: '', name: '', action: '', object: '' },
+        '69': { num: '69', initials: '', name: '', action: '', object: '' }
+    },
+    'pao70-79': {
+        '70': { num: '70', initials: '', name: '', action: '', object: '' },
+        '71': { num: '71', initials: '', name: '', action: '', object: '' },
+        '72': { num: '72', initials: '', name: '', action: '', object: '' },
+        '73': { num: '73', initials: '', name: '', action: '', object: '' },
+        '74': { num: '74', initials: '', name: '', action: '', object: '' },
+        '75': { num: '75', initials: '', name: '', action: '', object: '' },
+        '76': { num: '76', initials: '', name: '', action: '', object: '' },
+        '77': { num: '77', initials: '', name: '', action: '', object: '' },
+        '78': { num: '78', initials: '', name: '', action: '', object: '' },
+        '79': { num: '79', initials: '', name: '', action: '', object: '' }
+    },
+    'pao80-89': {
+        '80': { num: '80', initials: '', name: '', action: '', object: '' },
+        '81': { num: '81', initials: '', name: '', action: '', object: '' },
+        '82': { num: '82', initials: '', name: '', action: '', object: '' },
+        '83': { num: '83', initials: '', name: '', action: '', object: '' },
+        '84': { num: '84', initials: '', name: '', action: '', object: '' },
+        '85': { num: '85', initials: '', name: '', action: '', object: '' },
+        '86': { num: '86', initials: '', name: '', action: '', object: '' },
+        '87': { num: '87', initials: '', name: '', action: '', object: '' },
+        '88': { num: '88', initials: '', name: '', action: '', object: '' },
+        '89': { num: '89', initials: '', name: '', action: '', object: '' }
+    },
+    'pao90-99': {
+        '90': { num: '90', initials: '', name: '', action: '', object: '' },
+        '91': { num: '91', initials: '', name: '', action: '', object: '' },
+        '92': { num: '92', initials: '', name: '', action: '', object: '' },
+        '93': { num: '93', initials: '', name: '', action: '', object: '' },
+        '94': { num: '94', initials: '', name: '', action: '', object: '' },
+        '95': { num: '95', initials: '', name: '', action: '', object: '' },
+        '96': { num: '96', initials: '', name: '', action: '', object: '' },
+        '97': { num: '97', initials: '', name: '', action: '', object: '' },
+        '98': { num: '98', initials: '', name: '', action: '', object: '' },
+        '99': { num: '99', initials: '', name: '', action: '', object: '' }
     }
 }
