5f71eb74d86b2625b2f457996b2de9407221e2c5
PatrickO10 <patrickorth10@gmail.com>
2018-05-05 11:55:40 -0500

    adds style to the signin page

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,40 +1,53 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import serializeForm from 'form-serialize'
-import { Form, Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+import {
+    Form,
+    Button,
+    FormGroup,
+    FormControl,
+    ControlLabel,
+    Row,
+    Col,
+    Image
+} from 'react-bootstrap'
 
 class SignIn extends Component {
-	handleSubmit = (e) => {
+    handleSubmit = (e) => {
         e.preventDefault()
         const values = serializeForm(e.target, { hash: true })
         if (this.props.onSignIn)
             this.props.onSignIn(values)
     }
 
-	render() {
-		return (
-			<div>
-        		<Link className='close-sign-in' to='/'>Close</Link>
-        		<Form inline onSubmit={this.handleSubmit}>
-        			<FormGroup controlId='formCreateUser'>
-        				<ControlLabel>Email</ControlLabel>{' '}
-        				<FormControl
-        					type='text'
-        					name='email'
-        					placeholder='Email'
-        				/>
-        				<ControlLabel>Password</ControlLabel>
-        				<FormControl
-        					type="text"
-        					name="password"
-        					placeholder='Password'
-        				/>
-        			</FormGroup>
-        			<Button type="submit">Sign In</Button>
-        		</Form>
-      		</div>
-		)
-	}
+    render() {
+        return (
+            <div className='text-center signin-flex-container'>
+                <Form className='form-signin' onSubmit={this.handleSubmit}>
+                    <Link className='close-sign-in' to='/'>Close</Link>
+
+                    <Image src="http://placeholder.pics/svg/200x180"  width="300px" alt="Avocado" rounded responsive/>
+                    <h3>Sign In Please</h3>
+                    <FormGroup controlId='formCreateUser'>
+                        <FormControl
+                            type='text'
+                            name='email'
+                            placeholder='Email'
+                        />
+                        <FormControl
+                            type="text"
+                            name="password"
+                            placeholder='Password'
+                        />
+                    </FormGroup>
+
+                    <Button className="signin-button" type="submit">Sign In</Button>
+
+                </Form>
+            </div>
+
+        )
+    }
 }
 
 export default SignIn
